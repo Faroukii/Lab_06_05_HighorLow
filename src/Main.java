ac9561465c8ce6cf09492c01b90ba6ad30ad5dab
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-15 20:00:43 -0500

    random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        Random generator = new Random();
+        int val = generator.nextInt(10) +1;
+        int guess = -1;
+        System.out.println("Guess a number between 1 and 10 ");
+
+        {
+            System.out.print("Enter your guess: ");
+            guess = in.nextInt();
+            if (guess < val)
+            {
+                generator = new Random();
+
+                val = generator.nextInt(10) + 1;
+
+                System.out.println("Too low, please try again");
+            }
+            else if (guess > val)
+            {
+                generator = new Random();
+
+                val = generator.nextInt(10) + 1;
+                System.out.println("Too high, please try again");
+            }
+            else
+            {
+               generator = new Random();
+
+                val = generator.nextInt(10) + 1;
+
+                System.out.println("Your guess is correct!");
+            }
+        }
+
     }
 }
